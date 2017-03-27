2b1b609806b762759982e8e6bea5d6cc8a238716
Daniel Sikorski <dadi.sikorski@gmail.com>
2017-03-27 18:52:57 +0200

    eagle: update build fingerprint
    Change-Id: Ie2a632f201f07610d0460a5b0b875da020d17f12

--- xenonhd.mk
+++ xenonhd.mk
@@ -8,19 +8,31 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit some common XenonHD stuff.
 $(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
-# Fingerprint for eagle (from stock)
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D2303
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D2303/D2303:4.4.4/18.3.1.C.0.21/2n9_bg:user/release-keys
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D2303-user 18.3.1.C.0.21 2n9_bg release-keys"
+# Override Product Name for XenonHD
+BOARD_VENDOR := Sony
+PRODUCT_NAME := xenonhd_eagle
+PRODUCT_DEVICE := eagle
+PRODUCT_MODEL := XPERIA M2
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+TARGET_VENDOR := Sony
+
+# Use the latest approved GMS identifiers unless running a signed build
+# Fingerprint for eagle (from stock 5.1)
+ifneq ($(SIGN_BUILD),true)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Sony/D2303/D2303:5.1.1/18.6.A.0.182/1807889774:user/release-keys \
+    PRIVATE_BUILD_DESC="D2303-user 5.1.1 18.6.A.0.182 1807889774 release-keys"
+endif
+
+PRODUCT_GMS_CLIENTID_BASE := android-sonyericsson
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=D2303 \
+    PRODUCT_DEVICE=D2303 \
+    BUILD_PRODUCT=D2303
+
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=dadi11
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := D2302,D2303,D2305,D2306,D2403,D2406,eagle
-
-# Override Product Name for CyanogenMod
-PRODUCT_NAME		:= xenonhd_eagle
-PRODUCT_DEVICE		:= eagle
-PRODUCT_MODEL		:= Xperia M2
-PRODUCT_BRAND		:= Sony
-PRODUCT_MANUFACTURER	:= Sony
-
-PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=dadi11
